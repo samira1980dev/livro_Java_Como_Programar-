86e6b9207d5dee1e261bcc4281895e5fcf0336e5
Samira Marcela Roman <samira1980dev@gmail.com>
2022-09-20 17:03:26 -0300

    Class Average pag 96

--- JavaComoProgramar/src/Capitulo004/ClassAverage.java
+++ JavaComoProgramar/src/Capitulo004/ClassAverage.java
@@ -1,8 +1,39 @@
 package Capitulo004;
 
+import java.util.Scanner;
+
+// Figura 4.10: ClassAverage.java
+// Resolvendo o problema da média da classe usando a repetição controlada por sentinela
+
 public class ClassAverage {
 
 	public static void main(String[] args) {
+		
+		// cria Scanner para obter entrada a partir da janela de comando
+		Scanner input = new Scanner(System.in);
+		
+		int total = 0;
+		int gradeCounter = 0;
+		
+		System.out.print("Enter grade or -1 to quit: ");
+		int grade = input.nextInt();
+		
+		while(grade != -1) {
+			total = total + grade;
+			gradeCounter = gradeCounter + 1;
+			
+			System.out.print("Enter grade or -1 to quit: ");
+			grade = input.nextInt();
+		}
+		
+		if (gradeCounter != 0) {
+			double average = (double) total / gradeCounter;
+			
+			System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter, total);
+			System.out.printf("Class average is %.2f%n", average);
+		} else {
+			System.out.println("No grades were entered");
+		}
 
 	}
 
