5ff42c9a29026ec6732163880efc41f3d9641c49
Samira Marcela Roman <samira1980dev@gmail.com>
2022-10-25 15:06:40 -0300

    capitulo 05

---
+++ JavaComoProgramar/src/Capitulo005/ForCounter.java
@@ -0,0 +1,15 @@
+package Capitulo005;
+
+public class ForCounter {
+
+	public static void main(String[] args) {
+		
+		for (int counter = 1; counter <= 10; counter++) {
+			System.out.printf("%d ", counter);
+		}
+		
+		System.out.println();
+
+	}
+
+}
